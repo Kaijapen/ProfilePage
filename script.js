4f4cddd9660e87a6a54cd741a45c4ebaa5927d72
Kaijapen <104542328+Kaijapen@users.noreply.github.com>
2022-05-26 11:27:37 -0700

    new script file lol

---
+++ script.js
@@ -0,0 +1,20 @@
+function change1(){
+    document.querySelector('#req1').remove();
+    document.querySelector('#reqnum').innerText--;
+    
+}
+
+function change2(){
+    document.querySelector('#req2').remove();
+    document.querySelector('#reqnum').innerText--;
+}
+function change1(){
+    document.querySelector('#req1').remove();
+    document.querySelector('#reqnum').innerText--;
+    
+}
+
+function change2(){
+    document.querySelector('#req2').remove();
+    document.querySelector('#reqnum').innerText--;
+}
