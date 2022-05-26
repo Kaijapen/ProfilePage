7f36bca338e3e9a640847990b5ceb8310b88d31b
Kaijapen <104542328+Kaijapen@users.noreply.github.com>
2022-05-26 11:14:29 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function change1(){
+    document.querySelector('#req1').remove();
+    document.querySelector('#reqnum').innerText--;
+    
+}
+
+function change2(){
+    document.querySelector('#req2').remove();
+    document.querySelector('#reqnum').innerText--;
+}
+
